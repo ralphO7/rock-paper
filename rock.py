5533c16f64bd450e2d981d62f699a7e58a175afd
Ralph <ralphebib@gmail.com>
2023-11-04 22:00:47 +0100

    Change variables to better types

--- rock.py
+++ rock.py
@@ -1,7 +1,7 @@
 import random 
 
 
-mylist = ['scissors', "rock", "paper"]
+mylist = ('scissors', "rock", "paper")
 
 def get_input():
   try:
@@ -20,35 +20,24 @@ user_choice = get_input()
 
 
 def get_bot_choice():
-  rand = random.randint(0, 2)
-  computer_choice = mylist[rand]
+  computer_choice = random.choice(mylist)
   return computer_choice
 
 
-def get_res(value1, value2) -> str:
-
+def get_res(value1, value2):
   if value1 == value2:
-    return "Tie"
-  
-  elif value1 == "rock" and value2 == "scissors":
-    return "You win, the bot chose rock, and you chose scissors."
-  
-  elif value1 == "scissors" and value2 == "rock":
-    return "You win, the bot chose rock, and you chose scissors."
+    print("Tie")
 
-  elif value1 ==" paper" and value2 == "rock":
-    "You lose, the bot chose rock, and you chose paper."
-  
-  elif value1 =="rock" and value2 == "paper":
-    "You lose, the bot chose rock, and you chose paper."
+  elif value1 == "rock" and value2 == "scissors":
+    print("You win, the bot chose rock, and you chose scissors.")
+
+  elif value1 == "rock" and value2 == "paper":
+    print("You lose, the bot chose rock, and you chose paper.")
 
   elif value1 == "paper" and value2 == "scissors":
-    return "You lose, the bot chose paper, and you chose scissors."
-  
-  elif value1 == "scissors" and value2 == "paper":
-    return "You lose, the bot chose paper, and you chose scissors."
+    print("You lose, the bot chose paper, and you chose scissors.")
 
-  return "Invalid input or a tie occurred."
 
-print(get_bot_choice())
-print(get_res(value2=user_choice, value1=get_bot_choice()))
+result = get_res(value1=get_bot_choice(), value2=user_choice)
+
+print(result)
