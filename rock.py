bfecff24a3a3dbe52c66ce73a3de5fcc4cd6a149
Ralph <ralphebib@gmail.com>
2023-11-04 15:28:35 +0100

    finally

--- rock.py
+++ rock.py
@@ -1,16 +1,21 @@
 import random 
 
 
-mylist = ['pierre', "papier", "scisseau"]
+mylist = ['scissors', "rock", "paper"]
 
 def get_input():
   try:
     user_choice = None
 
     while not user_choice:
-      user_choice = str(input('select'))
-  except:
+      while user_choice not in mylist:
+        user_choice = str(input('select  '))
+  except ValueError:
     print('oops')
+  except :
+    print("bye")
+  
+  
 
 
 user_choice = get_input() 
@@ -18,14 +23,26 @@ user_choice = get_input()
 def get_bot_choice():
   rand = random.randint(0, 2)
   computer_choice = mylist[rand]
-  return "I selected {}".format(computer_choice)
+  return computer_choice
 
 #value1 : bot 
 #value2 : user
 
-def get_res(value1, value2):
+def get_res(value1, value2) -> str:
   if value1 == value2:
-    print("tie")
+    return "tie "
 
+  elif value1 == "rock" and value2 == "scissors":
+    return "you and idiot u lose"
+  
+  elif value1 == "rock" and value2 == "paper":
+    return "you won"
+  
+  elif value1 == "paper" and value2 == "scissors":
+    return "you won"
+  else : return
 
+print(get_bot_choice())  
+result = get_res(value2=user_choice, value1=get_bot_choice())
+print(result)
 
